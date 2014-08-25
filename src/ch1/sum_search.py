97cc409bd0fe7b9df5a79e3fc281b2657e90307c
cmiii <cmiii4@gmail.com>
2014-08-25 17:39:17 +0800

    add sum search alg and its test. It's the answer for Q2.3-7.

---
+++ src/ch1/sum_search.py
@@ -0,0 +1,31 @@
+
+def _bin_search(in_data, p, q, value):
+    if q <= p:
+        return -1
+    if q == p + 1:
+        if in_data[p] == value:
+            return p
+        else:
+            return -1
+    m = int((p + q) / 2)
+    if in_data[m] == value:
+        return m
+    elif in_data[m] > value:
+        return _bin_search(in_data, p, m, value)
+    else:
+        return _bin_search(in_data, m+1, q, value)
+
+
+def sum_search(in_data, sum_value):
+    found = False
+    tmp = in_data.copy()
+    tmp.sort()
+    length = len(tmp)
+    for i in range(0, length - 1):
+        target = sum_value - tmp[i]
+        ret = _bin_search(tmp, i+1, length, target)
+        if ret != -1:
+            found = True
+            print ("found (", i, ")", tmp[i], " + (", ret, ")", tmp[ret], " = ", sum_value)
+            break
+    return found
---
+++ src/ch1/test_other.py
@@ -0,0 +1,19 @@
+import fileinput
+
+
+def test_sum_search():
+    # get data from file
+    print("test sum search.")
+    in_data = []
+    for line in fileinput.input("../../data/sort1.dat"):
+        in_data.append(int(line))
+    sum_value = 128
+    print("in_data", in_data)
+    print("sum_value=", sum_value)
+
+    from sum_search import sum_search
+
+    ret = sum_search(in_data, sum_value)
+    print("ret = ", ret)
+
+    assert (ret)
